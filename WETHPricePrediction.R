bd0b67d1cf1cce7d4b2f8648de29650bac00b61d
Nehal Jain <nehaljain97@gmail.com>
2022-10-27 18:32:06 -0600

    Add files via upload

--- WETHPricePrediction.R
+++ WETHPricePrediction.R
@@ -1,17 +1,62 @@
 library(dplyr)
+library(ghql)
+library(jsonlite)
 library(ggplot2)
 
 # disable scientific notation
 options(scipen=999)
 
-#Reading the csv into R
-eth <- read.csv("C:\\Users\\Nehal\\OneDrive\\Documents\\MSBA_StudyMaterial\\Fall2022\\AdvancedDataAnalytics\\Project\\WETHDailyDayData.csv", header=TRUE, stringsAsFactors=TRUE)
+# Point to correct subgraph URL
+#Uniswap V3 GraphQL endpoint link - https://thegraph.com/hosted-service/subgraph/uniswap/uniswap-v3
+conn = GraphqlClient$new(
+  url = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3")
 
-#Data Cleaning using Dplyr
-eth = select(eth, -X)
-eth = select(eth, -token)
+# initialize a new query
+graphql_request = Query$new()
 
-head(eth)
+# make request
+graphql_request$query('mydata', '{
+        tokenDayDatas(orderBy: date, orderDirection: desc, first: 1000, where: {token: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"}) {
+            id
+            date
+            priceUSD
+            volume
+            volumeUSD
+            token {
+              symbol
+            }
+            open
+            high
+            low
+            close
+            totalValueLockedUSD
+            }
+        }
+')
+
+# Run query (pull data)
+wethToken = conn$exec(graphql_request$queries$mydata)
+# convert results to JSON
+wethToken = fromJSON(wethToken)
+# extract result
+wethToken = as.data.frame(wethToken$data$tokenDayDatas)
+
+# remove row names
+rownames(wethToken) = NULL
+
+# convert date to datetime
+wethToken = mutate(wethToken,
+                   date = as.POSIXct(as.numeric(wethToken$date), origin="1970-01-01"))
+
+wethToken$date <- as.Date(wethToken$date)
+
+#Cleaning the data by dropping column - token
+wethToken = select(wethToken, -token)
+
+head(wethToken)
+
+# write data 
+write.csv(wethToken,"C:\\Users\\Nehal\\OneDrive\\Documents\\MSBA_StudyMaterial\\Fall2022\\AdvancedDataAnalytics\\Project\\R_NEW_WETHDailyDayData.csv")
 
 # Visualize data
 p <- ggplot(eth, aes(x=date, y=priceUSD)) +
