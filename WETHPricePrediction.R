2d2520f227628b86af8e1de22e32eaf6f452c47e
Nehal Jain <nehaljain97@gmail.com>
2022-10-27 18:45:41 -0600

    Add files via upload

--- WETHPricePrediction.R
+++ WETHPricePrediction.R
@@ -1,3 +1,6 @@
+#Code to fetch the data from The Graph's endpoint for analysis. The transactions are filtered with Token ID = 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
+#This is the token ID for Wrapped ETH (WETH) : https://etherscan.io/token/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
+
 library(dplyr)
 library(ghql)
 library(jsonlite)
@@ -11,11 +14,12 @@ options(scipen=999)
 conn = GraphqlClient$new(
   url = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3")
 
-# initialize a new query
-graphql_request = Query$new()
+# Initializing a new query
+graphqlRequest = Query$new()
 
-# make request
-graphql_request$query('mydata', '{
+# Fetching top records ordered by date
+# Set query (which uses text input to specify ETH wallet addresses)
+graphqlRequest$query('newData', '{
         tokenDayDatas(orderBy: date, orderDirection: desc, first: 1000, where: {token: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"}) {
             id
             date
@@ -35,27 +39,27 @@ graphql_request$query('mydata', '{
 ')
 
 # Run query (pull data)
-wethToken = conn$exec(graphql_request$queries$mydata)
-# convert results to JSON
+wethToken = conn$exec(graphqlRequest$queries$newData)
+# JSON adjustment
 wethToken = fromJSON(wethToken)
-# extract result
+# Extract JSON to convert to a dataframe
 wethToken = as.data.frame(wethToken$data$tokenDayDatas)
 
-# remove row names
+# Remove row names
 rownames(wethToken) = NULL
 
-# convert date to datetime
+# Convert UNIX date to datetime
 wethToken = mutate(wethToken,
                    date = as.POSIXct(as.numeric(wethToken$date), origin="1970-01-01"))
-
+# Remove the timestamp and just keep date
 wethToken$date <- as.Date(wethToken$date)
 
-#Cleaning the data by dropping column - token
+#Cleaning the data by dropping column - 'token'
 wethToken = select(wethToken, -token)
 
 head(wethToken)
 
-# write data 
+# Write data into CSV
 write.csv(wethToken,"C:\\Users\\Nehal\\OneDrive\\Documents\\MSBA_StudyMaterial\\Fall2022\\AdvancedDataAnalytics\\Project\\R_NEW_WETHDailyDayData.csv")
 
 # Visualize data
