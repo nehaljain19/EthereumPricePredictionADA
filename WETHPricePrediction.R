765eb1fc695f7cb1740efaab216dc0e8319324df
Nehal Jain <nehaljain97@gmail.com>
2022-11-04 19:53:38 -0600

    Update WETHPricePrediction.R

--- WETHPricePrediction.R
+++ WETHPricePrediction.R
@@ -62,7 +62,7 @@ wethToken = subset(wethToken, low> 0)
 head(wethToken)
 
 # Write data into CSV
-write.csv(wethToken,"C:\\Users\\Nehal\\OneDrive\\Documents\\MSBA_StudyMaterial\\Fall2022\\AdvancedDataAnalytics\\Project\\WETHDailyDayData.csv")
+write.csv(wethToken,"WETHDailyDayData.csv")
 
 
 # Visualize data
