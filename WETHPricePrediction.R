4329cdc76e99200e8ad337c2ff2ea0f541e58e91
Nehal Jain <nehaljain97@gmail.com>
2022-11-04 19:52:27 -0600

    Add files via upload

--- WETHPricePrediction.R
+++ WETHPricePrediction.R
@@ -4,7 +4,7 @@
 library(dplyr)
 library(ghql)
 library(jsonlite)
-library(ggplot2)
+library(plotly)
 
 # disable scientific notation
 options(scipen=999)
@@ -57,11 +57,66 @@ wethToken$date <- as.Date(wethToken$date)
 #Cleaning the data by dropping column - 'token'
 wethToken = select(wethToken, -token)
 
+#Cleaning the data by dropping discrepancy value = 0
+wethToken = subset(wethToken, low> 0)
 head(wethToken)
 
 # Write data into CSV
-write.csv(wethToken,"WETHDailyDayData.csv")
+write.csv(wethToken,"C:\\Users\\Nehal\\OneDrive\\Documents\\MSBA_StudyMaterial\\Fall2022\\AdvancedDataAnalytics\\Project\\WETHDailyDayData.csv")
+
 
 # Visualize data
-ethTS <- ts(wethToken, frequency = 12)
-plot.ts(ethTS)
+# Visualizing a timeseries - Date vs Low using Plotly
+fig1 <- plot_ly(wethToken, type = 'scatter', mode = 'lines')%>%
+  add_trace(x = ~date, y = ~low)%>%
+  layout(showlegend = F, title='Time Series: Date vs Daily Low of WETH (in USD)',
+         xaxis = list(rangeslider = list(visible = T),
+                      rangeselector=list(
+                        buttons=list(
+                          list(count=1, label="1m", step="month", stepmode="backward"),
+                          list(count=6, label="6m", step="month", stepmode="backward"),
+                          list(count=1, label="YTD", step="year", stepmode="todate"),
+                          list(count=1, label="1y", step="year", stepmode="backward"),
+                          list(step="all")
+                        ))))
+
+fig1 <- fig1 %>%
+  layout(
+  xaxis = list(zerolinecolor = '#ffff',
+                 zerolinewidth = 2,
+                 gridcolor = 'ffff'),
+    
+    yaxis = list(zerolinecolor = '#ffff',
+                 zerolinewidth = 2,
+                 gridcolor = 'ffff'),
+    
+    plot_bgcolor='#e5ecf6', width = 900)
+fig1
+
+
+# Visualizing a timeseries - Date vs volumeUSD using Plotly
+fig2 <- plot_ly(wethToken, type = 'scatter', mode = 'lines')%>%
+  add_trace(x = ~date, y = ~volumeUSD)%>%
+  layout(showlegend = F, title='Time Series: Date vs Daily Volume of WETH (in USD)',
+         xaxis = list(rangeslider = list(visible = T),
+                      rangeselector=list(
+                        buttons=list(
+                          list(count=1, label="1m", step="month", stepmode="backward"),
+                          list(count=6, label="6m", step="month", stepmode="backward"),
+                          list(count=1, label="YTD", step="year", stepmode="todate"),
+                          list(count=1, label="1y", step="year", stepmode="backward"),
+                          list(step="all")
+                        ))))
+
+fig2 <- fig2 %>%
+  layout(
+    xaxis = list(zerolinecolor = '#ffff',
+                 zerolinewidth = 2,
+                 gridcolor = 'ffff'),
+    
+    yaxis = list(zerolinecolor = '#ffff',
+                 zerolinewidth = 2,
+                 gridcolor = 'ffff'),
+    
+    plot_bgcolor='#e5ecf6', width = 900)
+fig2
