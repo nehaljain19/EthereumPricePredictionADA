a89f92b4fe4012fc2498f422d6612fe6e09bb985
Nehal Jain <nehaljain97@gmail.com>
2022-12-02 18:51:47 -0700

    Add files via upload

--- PredictionScript.R
+++ PredictionScript.R
@@ -2,6 +2,7 @@
 library(caret)
 library(glmnet)
 library(MASS)
+library(tidyverse)
 
 
 # read data Please download Appointments.csv
@@ -13,10 +14,18 @@ weth$date<- as.Date(weth$date)
 weth$weekday <- weekdays(weth$date)
 weth$weekday <- as.factor(weth$weekday)
 weth = subset(weth, select = -c(X,date) )
+
+#Structure of dataframe
 str(weth)
+#Dimensions of dataframe
+dim(weth)
+#Descriptive statistics of dataframe
+summary(weth)
 
 
+cv_5 = trainControl(method = "cv", number = 5)
 
+####################################################################################
 #FIRST MODEL
 #Train and test split 
 set.seed(222)
@@ -30,8 +39,8 @@ custom <- trainControl(method = "repeatedcv", number = 10, repeats = 5, verboseI
 
 #fitting Elastic Net Regression model
 set.seed(1234)
-en <- train(Target ~. -Target - id, train, method='glmnet', tuneGrid =expand.grid(alpha=seq(0,1,length=10),
-            lambda = seq(0.0001,0.2,length=5)), trControl=custom)
+en <- train(Target ~. - id, train, method='glmnet', tuneGrid =expand.grid(alpha=seq(0,1,length=10),
+                                                                          lambda = seq(0.0001,0.2,length=5)), trControl=custom)
 en
 
 #mean validation score
@@ -43,71 +52,14 @@ plot(en, main = "Elastic Net Regression")
 #plotting important variables
 plot(varImp(en,scale=TRUE))
 
-##SECOND MODEL
-set.seed(42)
-cv_5 = trainControl(method = "cv", number = 5)
-
-default_idx = createDataPartition(weth$Target, p = 0.75, list = FALSE)
-default_trn = weth[default_idx, ]
-default_tst = weth[-default_idx, ]
-
-#Method 1
-def_elnet = train(
-  Target ~ . - Target - id, data = default_trn,
-  method = "glmnet",
-  trControl = cv_5
-)
-def_elnet
-
 calc_acc = function(actual, predicted) {
   mean(actual == predicted)
 }
 
+test$PredProb <- predict(en, newdata = test)
+FinalData <- test[order(-test$PredProb),]
+
 # test acc
-calc_acc(actual = default_tst$Target,
-         predicted = predict(def_elnet, newdata = default_tst))
+calc_acc(actual = test$Target,
+         predicted = ifelse(predict(en, newdata = test)>0.6,1,0))
 
-
-##Method 2
-# Build the model
-set.seed(123)
-elastic <- train(
-  Y ~., data = train, method = "glmnet",
-  family = "binomial",
-  trControl = trainControl("cv", number = 10),
-  tuneLength = 5
-)
-# Best tuning parameter
-elastic$bestTune
-
-elastic_model <- glmnet(x, y, family = "binomial", alpha = elastic$bestTune$alpha, lambda = elastic$bestTune$lambda) #lambda = NULL
-# Display regression coefficients
-#coef(lasso_model)
-
-#Make predictions on the test data
-
-probabilities <- elastic_model %>% predict(x.test)
-predicted.classes <- ifelse(probabilities > 0.5, "1", "0")
-#Model accuracy
-observed.classes <- test$Y
-mean(predicted.classes == observed.classes)
-
-
-##Method 3
-set.seed(123)
-lambda <- seq(-50, 50)
-alpha <- seq(-10, 10, length = 20)
-searchGrid = expand.grid(.alpha = alpha, .lambda = lambda)
-
-# Set training control
-tc <- trainControl(method = "repeatedcv",
-                   number = 5,
-                   repeats = 3)
-
-# Train the model
-en.model <- train(Target ~. -Target - id,
-                  data = train,
-                  method = "glmnet",
-                  tuneGrid = searchGrid,
-                  metric = "Accuracy",
-                  trControl = tc)
